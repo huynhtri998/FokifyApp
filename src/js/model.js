189c538e2420f5680279922c7394af2a12b3a9c9
huynhtri998 <huynhtri998@gmail.com>
2025-05-07 22:52:40 +0700

    Fokify project 7/05/2025

--- src/js/model.js
+++ src/js/model.js
@@ -8,13 +8,10 @@ export const loadRecipe = async function(id){
   try {
     const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
     const data = await res.json();
-
-    console.log(res,data)
     if (!res.ok) throw new Error(`${data.message} : ${res.status}`)
 
     let {recipe} = data.data;
     state.recipe = recipe;
-    console.log(state.recipe);
   }catch (err){
     alert(err);
   }
